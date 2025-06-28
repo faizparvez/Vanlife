90a464b7cb4aae9483a1ca057e8d3547f9445d66
faizparvez <151218353+faizparvez@users.noreply.github.com>
2025-06-28 23:28:20 +0530

    added the requireAuth function

---
+++ src/mirage/utils.js
@@ -0,0 +1,13 @@
+import { redirect } from "react-router-dom";
+
+export async function requireAuth() {
+  const isLoggedIn = false;
+  if (!isLoggedIn) {
+    const res = redirect("/login?message=You must login first.");
+    // Object.defineProperty(res, "body", { value: true });
+    res.body=true;
+    console.log(res);
+    throw res;
+  }
+  return null;
+}
