94060a1cca2787dfc897f40c7e8ebf82923efa11
faizparvez <151218353+faizparvez@users.noreply.github.com>
2025-07-01 22:53:15 +0530

    added the login, logout buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,26 @@
-import { NavLink, Link } from "react-router-dom";
+import { NavLink, Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-export default function Header() { 
+export default function Header() {
+  // const navigate = useNavigate();
+
+  function handleLogout() {
+    localStorage.removeItem("loggedin");
+    // navigate("/login", { replace: true });
+  }
+
   return (
     <header>
       <nav className="navbar-div">
-        <Link to="/" className="logo">#VANGO</Link>
+        <Link to="/" className="logo">
+          #VANGO
+        </Link>
         <div className="nav-links">
           <Link to="/host">Host</Link>
           <Link to="/about">About</Link>
           <Link to="/vans">Vans</Link>
+          <Link to="/login">Login</Link>
+          <button onClick={handleLogout}>Logout</button>
         </div>
       </nav>
     </header>
