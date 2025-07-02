2a8f04e11c19053a84820425baa4cfdea8824d6c
faizparvez <151218353+faizparvez@users.noreply.github.com>
2025-07-02 23:26:39 +0530

    created a redirect in the login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,32 @@
 import { useState } from "react";
-import { useLoaderData , Form } from "react-router-dom";
+import { useLoaderData, Form, redirect, useActionData, useNavigation } from "react-router-dom";
+import { loginUser } from "../mirage/api";
 
+export async function action({ request }) {
+  const formData = await request.formData();
+  const pathname = new URL(request.url).searchParams.get("redirectTo") || "/host";
+  // console.log(pathname);
 
+  const email = formData.get("email");
+  const password = formData.get("password");
+  // console.log( email , password )
 
-export async function action( {request} ) {
-  console.log(request);
-  const data = await request.formData();
-  console.log(data);
-
-  const email = data.get("email");
-  const password = data.get("password");
-
-  console.log( email , password )
-
-  return null;
+  try {
+    const hostData = await loginUser({ email, password });
+    // console.log(hostData);
+    if (hostData.token) {
+      localStorage.setItem("loggedin", true);
+      const dir = redirect(pathname);
+      dir.body = true;
+      return dir;
+    }
+  } catch (err) {
+    // console.log(err);
+    return err.message;
+  }
 }
 
-
-export function loader( {request} ){
+export function loader({ request }) {
   // console.log(request)
   // console.log(new URL(request.url));
   const mess = new URL(request.url).searchParams.get("message");
@@ -25,54 +34,58 @@ export function loader( {request} ){
   return mess;
 }
 
-
-
 export default function Login() {
-  
   const message = useLoaderData();
-  const [loginFormData,setLoginFormData]=useState({email:"",password:""})
+  const errorMessage = useActionData();
+  const navigation = useNavigation();
+  // console.log(navigation);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    // console.log(loginFormData);
-  }
+  // const [loginFormData, setLoginFormData] = useState({
+  //   email: "",
+  //   password: "",
+  // });
 
-  function handleChange(e){
-    setLoginFormData((prev)=>{
-        return (
-            {
-            ...prev,
-            [e.target.name]:e.target.value
-            }
-        )
-    })
-  }
+  // function handleSubmit(e) {
+  //   e.preventDefault();
+  //   // console.log(loginFormData);
+  // }
+
+  // function handleChange(e) {
+  //   setLoginFormData((prev) => {
+  //     return {
+  //       ...prev,
+  //       [e.target.name]: e.target.value,
+  //     };
+  //   });
+  // }
 
   return (
     <div className="login-container">
-      
-      <h1>Sign in to your account</h1>
-      
-      {message && <h2 className="red">{message}</h2>}
-
-      <Form method="post" className="login-form">
-        <input
-          name="email"
-          onChange={handleChange}
-          type="email"
-          placeholder="Email address"
-          value={loginFormData.email}
-        />
-        <input
-          name="password"
-          onChange={handleChange}
-          type="password"
-          placeholder="Password"
-          value={loginFormData.password}
-        />
-        <button>Log in</button>
-      </Form>
-    
+      <div className="login-form-wrapper">
+        <h1 className="login-title">Sign in to your account</h1>
+        {message && <h3 className="login-message">{message}</h3>}
+        {errorMessage && <h3 className="login-error">{errorMessage}</h3>}
+        <Form method="post" className="login-form" replace>
+          <input
+            name="email"
+            type="email"
+            placeholder="Email address"
+            className="login-input"
+          />
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            className="login-input"
+          />
+          <button
+            disabled={navigation.state === "submitting"}
+            className="login-button"
+          >
+            {navigation.state === "submitting" ? "Logging in" : "Log in"}
+          </button>
+        </Form>
+      </div>
     </div>
   );
 }
